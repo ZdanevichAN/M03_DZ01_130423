4a7aa34cbe1f035cfb94ec18143f988cb8cc6ec7
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 19:49:21 +0300

    ДЗ_01_Задача_04_найти max из трёх

---
+++ task04/MAXofFREE.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int count = 1;
+int max = 0;
+string numbers_name = "Введены следующие числа:";
+while(count<=3)
+{
+    Console.Write("Дорогой друг введи число № " + count+": ");
+    int num = int.Parse(Console.ReadLine());
+    numbers_name = numbers_name + " " + num;
+    
+    if (max<num)
+    {
+        max = num;
+    }
+
+    count++;
+}
+Console.WriteLine(numbers_name + " Максимальное из них: " + max);
