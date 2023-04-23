f91d5034a9f720184326c47c007e94f33280aa68
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 20:23:07 +0300

    ДЗ_01_Задача_08_от0доN вывести все четные

---
+++ task08/HowManyChet.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Сударь, введите люое число, а я Вам пушну четные что есть меж ним и нулём: ");
+int num_last = int.Parse(Console.ReadLine());
+string chet_name = " следующие числа являются четными:";
+int count = 0;
+while(count <=num_last)
+{
+    if (count%2 == 0)
+    {
+        chet_name = chet_name + ", " + count;
+    }
+    count++;
+}
+Console.WriteLine("От 0 до " + num_last + chet_name);
